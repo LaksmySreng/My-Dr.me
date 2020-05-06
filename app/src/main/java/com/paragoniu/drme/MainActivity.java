6e9b471b516e716e8157fc575ef8ff9271164569
DELL <lsreng2@paragoniu.edu.kh>
2020-05-06 22:15:21 +0700

    hello

--- app/src/main/java/com/paragoniu/drme/MainActivity.java
+++ app/src/main/java/com/paragoniu/drme/MainActivity.java
@@ -2,7 +2,9 @@ package com.paragoniu.drme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onButtonClick(View view){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
 }
